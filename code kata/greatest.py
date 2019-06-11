502f3800783ecc6272ba2125580366c13feb134b
Bhuvaneswaribai <51673016+Bhuvaneswaribai@users.noreply.github.com>
2019-06-11 23:20:25 +0530

    Update greatest.py

--- code kata/greatest.py
+++ code kata/greatest.py
@@ -1,9 +1,9 @@
-n1,n2,n3= input(),input(),input()
-if (n1 >n2) and (n1 >n3):
+n1,n2,c3= input(),input(),input()
+if (n1 >n2) and (n1 >c3):
    largest = n1
-elif (n2 >n1) and (n2 >n3):
+elif (n2 >n1) and (n2 >c3):
    largest = n2
 else:
-   largest = n3
+   largest = c3
 
 print(largest)
