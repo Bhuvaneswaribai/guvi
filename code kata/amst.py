0ac30a063cbc99d6e1d5a725fa71dc12d758be80
Bhuvaneswaribai <51673016+Bhuvaneswaribai@users.noreply.github.com>
2019-06-12 23:19:25 +0530

    Update amst.py

--- code kata/amst.py
+++ code kata/amst.py
@@ -3,9 +3,10 @@ g=int(input())
 s=list(str(g))
 for i in range(0, len(s)): 
   s[i] = int(s[i])
+  o = len(s)
 v=0
 for i in range(0,len(s)):
-  v=s[i]**3+v
+  v=s[i]**o+v
 if(v==g):
   print("yes")
 else:
