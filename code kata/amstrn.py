f641a08e0563f0ecd61800b6d785ed9ad119b450
Bhuvaneswaribai <51673016+Bhuvaneswaribai@users.noreply.github.com>
2019-06-12 23:27:14 +0530

    Create amstrn.py

---
+++ code kata/amstrn.py
@@ -0,0 +1,9 @@
+h,j=map(int,input().split())
+for f in range(h+1,j):
+    w=len(str(f))
+    h=str(f)
+    l=list(map(int,h))
+    li=[i**w for i in l]
+    k=sum(li)
+    if(k==f):
+        print(k)
