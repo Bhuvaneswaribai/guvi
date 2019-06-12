f288afce6203988c0b636a0cf31bf551dcf95d1d
Bhuvaneswaribai <51673016+Bhuvaneswaribai@users.noreply.github.com>
2019-06-12 16:40:19 +0530

    Create prime.py

---
+++ code kata/prime.py
@@ -0,0 +1,11 @@
+nu = int(input())
+if nu > 1: 
+   for i in range(2, nu): 
+       if (nu % i) == 0: 
+           print("no") 
+           break
+   else: 
+           print("yes") 
+  
+else: 
+   print("no") 
