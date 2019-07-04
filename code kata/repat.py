6f3377c8ac8182ba9b9f2a1a56be01131033ff0a
Bhuvaneswaribai <51673016+Bhuvaneswaribai@users.noreply.github.com>
2019-07-04 16:58:41 +0530

    Update repat.py

--- code kata/repat.py
+++ code kata/repat.py
@@ -3,10 +3,10 @@ f=int(input())
 z=list(map(int,input().split()))
 x=Counter(z)
 list=[]
-for i in x.items():
+for i in x.item():
    if(i[1]!=1):
       print(i[0],end="")
-for j in x.items():
+for j in x.item():
    list.append(j[1])
 if(max(list)==1):
   print("unique")
