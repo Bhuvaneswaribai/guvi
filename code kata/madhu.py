4903944652b0ddda8bd65e51643402c2c11fb54e
Bhuvaneswaribai <51673016+Bhuvaneswaribai@users.noreply.github.com>
2019-07-04 16:14:50 +0530

    Create madhu.py

---
+++ code kata/madhu.py
@@ -0,0 +1,20 @@
+def check(m,l):
+    a=l[0]
+    s = []
+    for i in range(len(m)):
+        for j in range(1,len(l)):
+            y=l[j]
+            if j==(len(l)-1) and a[i] == y[i]:
+                s.append(a[i])
+            elif a[i] == y[i]:
+                continue
+            else:
+                return s
+    return s
+n=int(input())
+l=[]
+for i in range(n):
+    s=input()
+    l.append(s)
+m=min(l)
+print("".join(check(m,l)))
